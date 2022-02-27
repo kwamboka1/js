1cc5a35a4d5b81d09de8382e28720116acb680b2
Zelipha <zeliphawambui756@gmail.com>
2022-02-27 12:21:39 +0300

    Counter works

--- index.js
+++ index.js
@@ -1,26 +1,22 @@
 //document.getElementById().innerText = 5
 // javascript
 
- 
- //initialization
- let saveEL = document.getElementById(save-el)
- let countEl = document.getElementById(count-el)
+//initialization
+let count = 0;
 
- let count = 0
+let countEl = document.getElementById("increment-btn");
+let counter = document.getElementById("count-el");
+let saveEL = document.getElementById("save-el");
 
- function increment(){
- 	//increment the count  variable when the button is clicked 
- 	count += 1
- 	//change the count-el in the HTML to reflect the new count
- 	countEl.innerText = count
+function increment() {
+  //increment the count  variable when the button is clicked
+  count++;
+  //change the count-el in the HTML to reflect the new count
+  counter.innerText = count;
+}
 
- }
-
- function save(){
- 	let countstr = count + " - " 
- 	saveEL.textContent += countstr   
- 	console.log(count)
- 
- }
-
-
+function save() {
+  let countstr = counter + " - ";
+  saveEL.textContent += countstr;
+  console.log(count);
+}
