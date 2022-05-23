b5a1a6aaf9e323be304003ed1d9c00d15ad1a33a
kwamboka1 <makorimildred@gmail.com>
2022-05-23 13:22:46 +0300

    [Update]: The counter app

--- Counter App/index.js
+++ Counter App/index.js
@@ -1,27 +1,26 @@
 //document.getElementById().innerText = 5
 // javascript
 
- 
- //initialization
- let saveEL = document.getElementById(save-el)
- let countEl = document.getElementById(increment-btn)
+//initialization
+let count = 0
 
+let countEl = document.getElementById("count-el")
+let counter = document.getElementById("reset")
+let saveEL = document.getElementById("save-el")
 
- let count = 0
+function increment() {
+  //increment the count  variable when the button is clicked
+  count +=1
+  //change the count-el in the HTML to reflect the new count
+  countEl.innerText = count
+}
 
- function increment(){
- 	//increment the count  variable when the button is clicked 
- 	count += 1
- 	//change the count-el in the HTML to reflect the new count
- 	countEl.innerText = count
+function save() {
+  let myCount = countEl.innerText + " - " //a variable that contains both the count and the dash separator
+  saveEL.textContent += myCount;
+  console.log(count)
 
- }
-
- function save(){
- 	let countstr = count + " - " 
- 	saveEL.textContent += countstr   
- 	console.log(count)
- 
- }
-
+  countEl.textContent = 0
+  count = 0
+}
 
